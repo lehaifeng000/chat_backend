ac3290c0347e070a3d2b838c98bfe613b6a78a9f
lehaifeng <lehaifeng000@163.com>
2025-04-08 21:59:16 +0800

    添加post响应

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, File, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pathlib import Path
@@ -25,3 +25,17 @@ def main():
 @app.get("/chat")
 def chat():
     return "mllm return msg";
+
+@app.post("/chat/send_message")
+async def send_message(text: str = Form(...), img: UploadFile = File(...)):
+    print("收到消息：",text)
+    try:
+        image = await img.read()
+            # 你可以在这里保存文件到服务器
+            # 例如:
+        with open(f"uploads/{img.filename}", "wb") as f:
+            f.write(image)
+        print("receive img: ", img.filename)
+    except Exception as e:
+        print(e)
+    return "yes, this is response"
